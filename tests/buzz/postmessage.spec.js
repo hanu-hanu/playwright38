909845bfb5c45628cf4f71b0f6c2dca732d36b88
Hanumanth <hanumanths8981@gmail.com>
2026-08-19 14:08:28 +0530

    save

--- tests/buzz/postmessage.spec.js
+++ tests/buzz/postmessage.spec.js
@@ -8,7 +8,9 @@ test("verify", async ({page}) => {
  await page.getByRole('textbox', { name: 'Password' }).fill(process.env.APP_PASSWORD)
  await page.getByRole('button', { name: 'Login' }).click()
  
- await expect(page).toHaveURL("https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index").catch.
+ await expect(page).toHaveURL("https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index")...expect.any
+
+ 
 
 
  
