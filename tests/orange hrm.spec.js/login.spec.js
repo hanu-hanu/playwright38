c90471638cc1c713cafe47c0d0f2709d7a0c3e68
Hanumanth <hanumanths8981@gmail.com>
2026-08-19 12:06:52 +0530

    added .env file

--- tests/orange hrm.spec.js/login.spec.js
+++ tests/orange hrm.spec.js/login.spec.js
@@ -4,9 +4,9 @@ import logindata from "../../testdata/login.json"
 test('verify login with valid credentials', async ({ page }) => {
   await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
   await page.getByRole('textbox', { name: 'Username' }).click();
-  await page.getByRole('textbox', { name: 'Username' }).fill(logindata.username);
+  await page.getByRole('textbox', { name: 'Username' }).fill(process.env.APP_USERNAME);
   await page.getByRole('textbox', { name: 'Password' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).fill(logindata.password);
+  await page.getByRole('textbox', { name: 'Password' }).fill(process.env.APP_PASSWORD);
   await page.getByRole('button', { name: 'Login' }).click();
 
 });
