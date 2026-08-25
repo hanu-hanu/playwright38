b78a5976bf330c39aedd7344f55d5e32b2a157e1
Hanumanth <hanumanths8981@gmail.com>
2026-08-25 12:07:28 +0530

    commit

--- tests/ORANGEHRM/Admin.spec.js
+++ tests/ORANGEHRM/Admin.spec.js
@@ -20,54 +20,7 @@ test('add a new job category', async ({ page }) => {
 });
 
 
-test('add a job recruitment ', async ({ page }) => {
-  
-  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-  await page.getByRole('textbox', { name: 'Username' }).click();
-  await page.getByRole('textbox', { name: 'Username' }).fill(process.env.APP_USERNAME);
-  await page.getByRole('textbox', { name: 'Password' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).fill(process.env.APP_PASSWORD);
-  await page.getByRole('button', { name: 'Login' }).click();
-  await page.getByRole('link', { name: 'Recruitment' }).click();
-  await page.getByRole('button', { name: ' Add' }).click();
-  await page.getByRole('textbox', { name: 'First Name' }).click();
-  await page.getByRole('textbox', { name: 'First Name' }).fill('divya');
-  await page.getByRole('textbox', { name: 'Last Name' }).click();
-  await page.getByRole('textbox', { name: 'Last Name' }).fill('m');
-  await page.locator('i').nth(5).click();
-  await page.getByText('Senior Software Test Engineer 1786630898508').click();
-  await page.getByRole('textbox', { name: 'Type here' }).first().click();
-  await page.getByRole('textbox', { name: 'Type here' }).first().fill('reachme@gmail.com');
-  await page.getByRole('textbox', { name: 'Type here' }).nth(1).click();
-  await page.getByRole('textbox', { name: 'Type here' }).nth(1).fill('9191919191');
-  await page.getByRole('textbox', { name: 'Enter comma seperated words...' }).click();
-  await page.getByRole('textbox', { name: 'Enter comma seperated words...' }).fill('reach me');
-  await page.locator('.oxd-icon.bi-calendar').click();
-  await page.getByText('13').click();
-  await page.locator('textarea').click();
-  await page.locator('textarea').fill('reach me ');
-  await page.locator('.oxd-icon.bi-check').click();
-  await page.getByRole('button', { name: 'Save' }).click();
-  await page.getByRole('button', { name: 'Shortlist' }).click();
-  await page.getByRole('button', { name: 'Save' }).click();
-  await page.getByRole('button', { name: 'Reject' }).click();
-  await page.getByRole('button', { name: 'Save' }).click();
-  await expect(page.locator('div').filter({ hasText: 'Application StageNamedivya' }).nth(3)).toBeVisible();
-});
 
-test('verify delete a job category', async ({ page }) => {
-  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-  await page.getByRole('textbox', { name: 'Username' }).click();
-  await page.getByRole('textbox', { name: 'Username' }).fill(process.env.APP_USERNAME);
-  await page.getByRole('textbox', { name: 'Password' }).click();
-  await page.getByRole('textbox', { name: 'Password' }).fill(process.env.APP_PASSWORD);
-  await page.getByRole('button', { name: 'Login' }).click();
-  await page.getByRole('link', { name: 'Admin' }).click();
-  await page.getByRole('listitem').filter({ hasText: 'Job' }).locator('i').click();
-  await page.getByRole('listitem').filter({ hasText: /^Job Categories$/ }).click();
-  await page.locator('div:nth-child(11) > .oxd-table-row > div:nth-child(3) > .oxd-table-cell-actions > button').first().click();
-  await page.getByRole('button', { name: ' Yes, Delete' }).click();
-});
 
 
 test("test", async ({ page }) => {
---
+++ tests/git/jobrequirement.spec.js
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+test('add a job recruitment ', async ({ page }) => {
+  
+await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
+await page.locator('//input[@name="username"]').fill(process.env.APP_USERNAME);
+await page.locator('//input[@name="password"]').fill(process.env.APP_PASSWORD);
+await page.locator('//button[@type="submit"]').click();
+await page.locator('//div[@id="app"]').click();
+
+
+
+
+  
+});
